f74d24c9b1fceb0847341f652f36b4111ed2e0dd
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-06-29 16:51:02 +0530

    Created basic structure of Profile

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -3,8 +3,49 @@ import Image from "next/image"
 const Profile = () => {
     return (
         <div className="flex flex-col">
-            Profile
-            
+            <div>
+                <div className="
+                    absolute 
+                    top-0 
+                    left-0 
+                    w-full
+                    ">
+                    <Image 
+                        src="/test/banner.png" 
+                        width="200" height="200" 
+                        alt="Banner" 
+                        className="w-full h-[300px]" 
+                    />
+                </div>
+                <div className="flex flex-row">
+                    <div className="
+                        z-10 
+                        relative 
+                        ml-4 
+                        mt-56 
+                        md:ml-20
+                        ">
+                        <Image 
+                            alt="user image" 
+                            src="/test/leomord.png" 
+                            width="100" 
+                            height="100" 
+                            className="w-40 h-40 rounded-full"
+                        />
+                    </div>
+
+                    <div className="z-10 relative"> 
+                        <div className="mt-[12.5em] md:mt-[8.3em] text-2xl md:text-4xl">
+                            Swapnil Pant
+                        </div>
+
+                        <div className="text-xl md:text-2xl text-gray-600">
+                            Hustler
+                        </div>
+                    </div>
+                
+                </div>
+            </div>
         </div>
     )
 }
