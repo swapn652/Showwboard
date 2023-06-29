2e39707bfa3027c91e228614f461a0e1932208a7
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-06-29 15:39:22 +0530

    Basic setup

---
+++ app/components/Profile.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+
+const Profile = () => {
+    return (
+        <div className="flex flex-col">
+            Profile
+            
+        </div>
+    )
+}
+
+export default Profile
